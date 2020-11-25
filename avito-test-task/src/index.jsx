f3a8d61e9da812e46a72fa3b62b8149f3d723993
Maxim Samorukov <maxim.samorukov@gmail.com>
2020-11-25 23:51:02 +0300

    update readme

--- avito-test-task/src/index.jsx
+++ avito-test-task/src/index.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './modules/styles/index.css';
 import Board from './modules/Board';
-// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
